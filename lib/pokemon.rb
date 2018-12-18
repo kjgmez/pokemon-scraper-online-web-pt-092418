16c12947beff0abad92e6dc14f4cc5855a967aee
Kevin J Gomez <kjgmez1@gmail.com>
2018-12-18 15:03:57 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,6 +22,6 @@ class Pokemon
     Pokemon.new(id: id, name: pname, type: ptype, db: db)
   end
   def self.alter_hp(hp, db)
-    db.execute("UPDATE hp FROM pokemon WHERE name = Pikachu")
+    db.execute("UPDATE pokemon SET hp = #{hp} WHERE name = Pikachu")
   end
 end
